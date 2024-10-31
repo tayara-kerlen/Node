7e129bdee48dd6c6c9d5fab93ef31a8c8a6d23c1
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2024-10-31 17:00:07 -0300

    Hash com BCrypt

---
+++ bcrypt/app.js
@@ -0,0 +1,30 @@
+// Criptografa (hash) a senha com BCrypt
+
+// Criptografia pode ser revertida (descriptografar).
+
+// Hash não pode reverter, é uma via de mão única.
+// - A senha informada pelo usuário é convertida em hash utilizando a mesma técnica do cadastro e comparada com a senha que já está armazenada no Banco de Dados.
+
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;  // Número de "camadas" de segurança
+const senhaInformada = "123456";
+
+bcrypt.hash(senhaInformada, saltRounds, (erro, hashGerado) => {
+    // Se der erro, mostra uma mensagem
+    if (erro) {
+        throw erro;
+    }
+
+    console.log('Hash Bcrypt: ', hashGerado);
+    
+    // Verifica/compara a senha
+    bcrypt.compare(senhaInformada, hashGerado, (erro, resultado) => {
+        // Se der erro, mostra uma mensagem
+        if (erro) {
+            throw erro;
+        }
+
+        console.log("Senha correta: ", resultado);
+    });
+});
